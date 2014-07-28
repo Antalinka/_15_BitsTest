44f44ca8843f49f8a88868cf144d7809e50fc352
Evgenia Tretiakova <yantalinka@gmail.com>
2014-07-28 22:35:50 +0300

    init2

---
+++ _15_Superman_Bita/_15_Superman_BitaTests/_15_Superman_BitaTests.m
@@ -0,0 +1,34 @@
+//
+//  _15_Superman_BitaTests.m
+//  _15_Superman_BitaTests
+//
+//  Created by Exo-terminal on 3/27/14.
+//  Copyright (c) 2014 Evgenia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _15_Superman_BitaTests : XCTestCase
+
+@end
+
+@implementation _15_Superman_BitaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
